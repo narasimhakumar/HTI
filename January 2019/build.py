ba5ad8f8f96fa0149149101813c2a4e5a77ab4aa
narasimhakumar <narasimha.kumar10@gmail.com>
2019-02-20 00:23:49 -0800

    Delete build.py

--- January 2019/build.py
+++
@@ -1,8 +0,0 @@
-var = input('Please enter Y(es) or N(o): ')
-if 'y' in var[0].lower() or 'yes' in var.lower():
-    print ('You entered Yes')
-else:
-    if var[0].lower() == 'n':
-        print ('You entered No')
-
-print('You really entered ' + str(var))
